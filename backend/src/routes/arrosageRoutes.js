91e12dc657d57b8726c3d21e9b87dae8ed76dfbf
Manon Avaullée <avaullee.manon@gmail.com>
2024-12-05 12:06:12 +0100

    UPDATE : CD - Port(6)

--- backend/src/routes/arrosageRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const arrosageController_1 = require("../controllers/arrosageController");
-const router = (0, express_1.Router)();
-router.get('/', arrosageController_1.getAllArrosages);
-exports.default = router;
